fc0f6bbf5a222c4e2001f7442a63e9278731f429
Zina Mohamed <zina.a.mohamed@gmail.com>
2021-01-20 14:34:08 -0800

    Step 1: Design cart complete 🎉

---
+++ cart/cart-data.js
@@ -0,0 +1,18 @@
+export const cart = [
+    {
+        id: 1,
+        quantity: 10
+
+    },
+
+    {
+        id: 2,
+        quantity: 6
+
+    },
+    {
+        id: 3,
+        quantity: 11
+
+    }
+];
